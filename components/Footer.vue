63de7c1dc236f536a78314ba0705a3cdf7591ad2
Lo Van Long <longlv@v-auto.com.vn>
2021-04-12 17:20:33 +0700

    fix

--- components/Footer.vue
+++ components/Footer.vue
@@ -160,6 +160,34 @@
 
       gtag('config', 'G-2G9ZM8JQ20')
     </script>
+    <!-- Facebook Pixel Code -->
+    <script>
+      !(function (f, b, e, v, n, t, s) {
+        if (f.fbq) return
+        n = f.fbq = function () {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments)
+        }
+        if (!f._fbq) f._fbq = n
+        n.push = n
+        n.loaded = !0
+        n.version = '2.0'
+        n.queue = []
+        t = b.createElement(e)
+        t.async = !0
+        t.src = v
+        s = b.getElementsByTagName(e)[0]
+        s.parentNode.insertBefore(t, s)
+      })(
+        window,
+        document,
+        'script',
+        'https://connect.facebook.net/vi_VN/fbevents.js'
+      )
+      fbq('init', '241950547678859')
+      fbq('track', 'PageView')
+    </script>
   </div>
 </template>
 <script>
