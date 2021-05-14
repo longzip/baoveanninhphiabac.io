2ff7991cb23be15a7c1dfe81ad77f0a1a4e03580
Lo Van Long <longlv@v-auto.com.vn>
2021-05-14 22:32:20 +0700

    generate

--- docs/static/1618560277/tuyen-dung/tuyen-bao-ve-vinh-yen-can-gap-20-nhan-vien-bao-ve-ca-ngay-va-toi/state.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:docs/static/1621005692/tuyen-dung/tuyen-bao-ve-vinh-yen-can-gap-20-nhan-vien-bao-ve-ca-ngay-va-toi/state.js
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002Fstatic\u002F1621005692",layout:"default",error:null,serverRendered:true,routePath:"\u002Ftuyen-dung\u002Ftuyen-bao-ve-vinh-yen-can-gap-20-nhan-vien-bao-ve-ca-ngay-va-toi",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002F"}}}}("\u002F"));
-=======
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002Fstatic\u002F1618560277",layout:"default",error:null,serverRendered:true,routePath:"\u002Ftuyen-dung\u002Ftuyen-bao-ve-vinh-yen-can-gap-20-nhan-vien-bao-ve-ca-ngay-va-toi",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002F"}}}}("\u002F"));
->>>>>>> b2592e3d12b71cf7c8f2aeb1d899ce2ab4b4ceda:docs/static/1618560277/tuyen-dung/tuyen-bao-ve-vinh-yen-can-gap-20-nhan-vien-bao-ve-ca-ngay-va-toi/state.js
--- docs/static/1621005692/tuyen-dung/state.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:docs/static/1621005692/tuyen-dung/state.js
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002Fstatic\u002F1621005692",layout:"default",error:null,serverRendered:true,routePath:"\u002Ftuyen-dung",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002F"}}}}("\u002F"));
-=======
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002Fstatic\u002F1618560277",layout:"default",error:null,serverRendered:true,routePath:"\u002Ftuyen-dung",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fbaoveanninhphiabac.ga\u002F"}}}}("\u002F"));
->>>>>>> b2592e3d12b71cf7c8f2aeb1d899ce2ab4b4ceda:docs/static/1618560277/tuyen-dung/state.js
